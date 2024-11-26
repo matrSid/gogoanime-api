43e408dbbb941f6d4f0e975018d0eccd2f5bea15
Bhoopendra Sikarwar <84861562+codersikarwar@users.noreply.github.com>
2024-11-26 19:08:24 +0530

    genre

--- scraper.py
+++ scraper.py
@@ -48,10 +48,7 @@ async def get_anime(anime_id: str) -> Dict[str, str]:
     img_url = parser.css_first(".anime_info_body_bg img").attributes["src"]
     about = parser.css_first(".anime_info_body_bg div:nth-of-type(1)").text()
     name = parser.css_first("div.anime_info_body_bg h1").text()
-
-    genre_section = parser.css_first("p:contains('Genre:')")
-    genre_links = genre_section.css("a")
-    genre = ", ".join([link.text for link in genre_links])
+    gere = parser.css_first("div.anime_info_bofy_bg p:nth-of-type(4)").text()
 
     released_text = parser.css_first("div.anime_info_body_bg p:nth-of-type(5)").text()
     released = released_text.split(":")[-1].strip()
